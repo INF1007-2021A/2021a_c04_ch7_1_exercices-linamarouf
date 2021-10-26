fa8da3f6b46f71ab171c15c6529a6f2e24a19a99
linamarouf <90852417+linamarouf@users.noreply.github.com>
2021-10-26 11:21:49 -0400

    1

--- exercice.py
+++ exercice.py
@@ -6,9 +6,16 @@
 
 
 # TODO: Définissez vos fonction ici
+import math
+
+
+def volumemasse(a=2, b=4, c=6, massevolumique=10):
+    v = (4/3) * math.pi * a*b*c
+    m = v * massevolumique
+    return v, m
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
 
-    pass
+    print(volumemasse())
